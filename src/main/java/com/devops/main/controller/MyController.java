23fa0354cd027b2a46dddeecbe9e71d7b666b885
Hamid Hussain <hamideraqui0786@gmail.com>
2023-01-02 19:56:18 +0530

    Adding About

--- src/main/java/com/devops/main/controller/MyController.java
+++ src/main/java/com/devops/main/controller/MyController.java
@@ -1,5 +1,8 @@
 package com.devops.main.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,17 @@ public class MyController {
 	@GetMapping("/")
 	public String welcome() {
 		return "Welcome to rest api...it's working";
+	}
+	
+	@GetMapping("/about")
+	public Map<String,String> about() {
+		Map<String, String> map = new HashMap<>();
+		
+		map.put("name", "Md Hamid Hussain");
+		map.put("address", "Shivganj, Narkatiaganj");
+		map.put("mobile", "9354804504");
+		
+		return map;
+		
 	}
 }
